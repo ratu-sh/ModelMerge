331080824165902344b6f7cb7bd7036e0923afb2
yym68686 <yym68686@outlook.com>
2024-05-23 04:34:50 +0800

    Fixed the bug: ModuleNotFoundError: No module named 'ModelMerge.models.config'

--- src/ModelMerge/plugins/__init__.py
+++ src/ModelMerge/plugins/__init__.py
@@ -6,4 +6,4 @@ from .image import generate_image
 from .run_python import *
 from .config import *
 
-__all__ = ['get_city_tarvel_info', 'get_date_time_weekday', 'get_version_info', 'get_search_results', 'Web_crawler', 'generate_image', 'run_python_script']
+__all__ = ['get_city_tarvel_info', 'get_date_time_weekday', 'get_version_info', 'get_search_results', 'Web_crawler', 'generate_image', 'run_python_script', 'PLUGINS']
