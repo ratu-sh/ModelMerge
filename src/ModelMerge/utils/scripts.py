3a808344849ed5c9e708ceb5bf836d6a49df3d83
yym68686 <yym68686@outlook.com>
2024-06-25 22:18:44 +0800

    Fixing the bug in the reply where the file is not an image is caused by not checking if the image file is None.

--- src/ModelMerge/utils/scripts.py
+++ src/ModelMerge/utils/scripts.py
@@ -74,7 +74,7 @@ def get_image_message(image_url, message, engine = None):
 def Document_extract(docurl, docpath=None, engine = None):
     filename = docpath
     text = None
-    if docpath and "paper.pdf" != docpath:
+    if docpath and docurl and "paper.pdf" != docpath:
         filename = get_doc_from_url(docurl)
         docpath = os.getcwd() + "/" + filename
     if filename and filename[-3:] == "pdf":
